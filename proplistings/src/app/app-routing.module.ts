cddad3f2a822fc3bb3bebbe2724e21f9b23dc1c5
matsubrasil <matsuboy@outlook.com>
2017-07-03 23:11:18 -0300

    routing navbar

--- proplistings/src/app/app-routing.module.ts
+++ proplistings/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'listings', component: ListingsComponent },
+  { path: 'add-listing', component: AddListingComponent },
+  { path: 'home', component: HomeComponent },
+
 ];
 
 @NgModule({
