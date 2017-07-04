7dda2297110ff557c323da666111b8a0064fbe09
matsubrasil <matsuboy@outlook.com>
2017-07-03 22:14:55 -0300

    some components

--- proplistings/src/app/app.module.ts
+++ proplistings/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+import { EditListingComponent } from './components/edit-listing/edit-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ListingsComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
   ],
   imports: [
     BrowserModule,
